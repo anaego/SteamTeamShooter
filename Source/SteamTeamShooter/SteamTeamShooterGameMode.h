a2f40da0f5980075d24db786d587d4755b730805
anaego <anastas.ego@gmail.com>
2021-11-16 23:27:52 +0300

    Initial commit

---
+++ Source/SteamTeamShooter/SteamTeamShooterGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SteamTeamShooterGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASteamTeamShooterGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASteamTeamShooterGameMode();
+};
+
+
+
